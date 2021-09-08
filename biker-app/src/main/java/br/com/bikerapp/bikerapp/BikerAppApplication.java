885ffdd015ee6b43319a7ffa926e7d3f424bd865
deivis <deivis.sperotto@gmail.com>
2021-09-08 16:15:29 -0300

    Setup Spring Project

---
+++ biker-app/src/main/java/br/com/bikerapp/bikerapp/BikerAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.bikerapp.bikerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerAppApplication.class, args);
+	}
+
+}
---
+++ biker-app/src/test/java/br/com/bikerapp/bikerapp/BikerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bikerapp.bikerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
